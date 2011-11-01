098bee923f8d4eb9b393061264d705129d233408
Ben Aston <ben.aston@justgiving.com>
2011-11-01 02:39:21 +0000

    Minor mods.

---
+++ src/NServiceLocator.Test.Fast/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace NServiceLocator.Test.Fast
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class Test
+    {
+        [Test]
+        public void Test()
+        {
+            var s = new 
+        }
+    }
--- src/NServiceLocator/IServiceLocator.cs
+++ src/NServiceLocator/IServiceLocator.cs
@@ -7,11 +7,13 @@
     ///   Responsible for defining the interface for types 
     ///   providing service-location functionality.
     /// </summary>
-    public interface IServiceLocator
+    public interface IServiceLocator<out TActivationContext>
     {
         void BindToSelf(Type obj);
 
         void BindToSelf<T>();
+
+        void BindToMethod<T>(Func<TActivationContext, T> func);
 
         void BindToInterface<TImplementation, TInterface>(TImplementation type, TInterface tInterface)
             where TImplementation : TInterface where TInterface : Type;
